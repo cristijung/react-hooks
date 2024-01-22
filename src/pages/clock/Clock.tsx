3df147588edf289446fae4be874ab9fb4b242048
Cristina Jung <cristijung@gmail.com>
2024-01-22 11:55:17 -0300

    comentarios de explicação add

--- src/pages/clock/Clock.tsx
+++ src/pages/clock/Clock.tsx
@@ -1,22 +1,36 @@
 import { useState, useEffect } from 'react';
+//O componente faz uso dos hooks useState e useEffect do React. 
+//useState é utilizado para gerenciar o estado local do componente, 
+//enquanto useEffect é usado para realizar operações secundárias 
+//(efeitos colaterais) durante o ciclo de vida do componente.
+
 
 interface ClockProps {
   timeZone: string;
 }
 
 const Clock = ({ timeZone }: ClockProps) => {
+    // Estado local para armazenar a data/hora atual
   const [time, setTime] = useState(new Date());
 
+    // Efeito colateral para atualizar a hora a cada segundo
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
+    // Função de limpeza para remover o intervalo quando o componente é desmontado
     return () => clearInterval(intervalId);
-  }, []);
+  }, []); // O array vazio assegura que o efeito é executado apenas uma vez durante a montagem
 
-  const formattedTime = time.toLocaleTimeString('en-US', { timeZone });
+  // Formata a hora de acordo com a zona de tempo fornecida
+//  useState é utilizado para criar uma variável de estado time que armazena a data e hora atuais.
+//useEffect é utilizado para realizar operações secundárias. Neste caso, cria um intervalo que atualiza o estado time a cada segundo. 
+//O intervalo é limpo quando o componente é desmontado para evitar vazamentos de memória.
+//A função toLocaleTimeString é usada para formatar a hora de acordo com a zona de tempo fornecida.
+  const formattedTime = time.toLocaleTimeString('pt-BR', { timeZone });
 
+  // Retorna a representação visual do componente
   return (
     <div>
       <p>Hora ({timeZone}): {formattedTime}</p>
--- src/pages/counter/Counter.tsx
+++ src/pages/counter/Counter.tsx
@@ -1,3 +1,7 @@
+//O componente faz uso dos hooks useState e useEffect do React. 
+//useState é utilizado para gerenciar o estado local do componente (no caso, o valor do contador), 
+//enquanto useEffect é usado para realizar operações secundárias durante o ciclo de vida do componente
+
 import { useState, useEffect } from 'react';
 
 interface CounterProps {
@@ -5,15 +9,17 @@ interface CounterProps {
 }
 
 const Counter = ({ initialCount }: CounterProps) => {
+    // Estado local para armazenar o valor do contador
   const [count, setCount] = useState(initialCount);
 
+  // Efeito colateral para exibir mensagens no console durante a montagem e desmontagem do componente
   useEffect(() => {
     console.log('Componente montado');
 
     return () => {
       console.log('Componente desmontado');
     };
-  }, []);
+  }, []); // O array vazio assegura que o efeito é executado apenas uma vez durante a montagem
 
   return (
     <div>
