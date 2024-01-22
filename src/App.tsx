7f8333294ad901338cde3be2d552958a2520e718
Cristina Jung <cristijung@gmail.com>
2024-01-22 10:42:39 -0300

    estrutura com layout default

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Default from './pages/default/Default';
 
 function App() {
   return (
   <>
+    <Default/>
   </>
   );
 }
---
+++ src/pages/default/Default.tsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import NavBar from "../../components/navbar/NavBar";
+
+export default function Default() {
+    return(
+        <>
+        <Header />
+        <NavBar />
+        <Footer />
+        </>
+    )
+}
